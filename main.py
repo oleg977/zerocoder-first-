c81fac2eb516953087cf863e33b60c5ae63cdc8f
oleg977 <0284612g@gmail.com>
2025-03-17 18:42:44 +0300

    новый бот
    начало разработки бота

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print ("hello World")
+import telebot
+import time
+bot = telebot.TeleBot(TOKEN)
+@bot.message_handler(commands =["start"])
+def send_velcome(message);
+   global chat_id
+   bot.reply_to(message, "Привет. Я твой бот-помощник")
+
