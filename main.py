bdff22f5fb589141fd0d831c5e75422cf511bf9e
oleg977 <0284612g@gmail.com>
2025-03-18 15:58:13 +0300

    add new class

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-import telebot
-import time
-bot = telebot.TeleBot(TOKEN)
-@bot.message_handler(commands =["start"])
-def send_velcome(message);
-   global chat_id
-   bot.reply_to(message, "Привет. Я твой бот-помощник")
 
+
+
+class Warrior():
+    def __init__(self, name, power, endurance, hair_color):
+      self.name = name
+      self.power = power
+      self.endurance = endurance
+      self.hair_color = hair_color
+    def sleep(self):
+        print(f"{self.name} лег спать")
+        self.endurance += 2
+    def eat(self):
+        print(f'{self.name} сел кушать')
+        self.power +=1
+    def hit(self):
+        print(f'{self.name} бьёт кого то')
+        self.endurance -=6
+    def work(self):
+        print(f"{self.name} гуляет")
+    def info(self):
+        print(f' имя воина - {self.name}')
+        print(f"цвет волос воина - {self.hair_color}")
+        print(f"сила воина - {self.power}")
+        print(f"выносливость воина - {self.endurance}")
