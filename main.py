bb31c96de02a7bb3e94ba7e25cc00561d1fdb16d
oleg977 <0284612g@gmail.com>
2025-03-24 20:46:57 +0300

    add class animal

--- main.py
+++ main.py
@@ -1,41 +1,27 @@
-class User:
-    def __init__(self, user_id, name):
-        self._user_id = user_id  # Уникальный идентификатор пользователя
-        self._name = name        # Имя пользователя
-        self._access_level = 'user'  # Уровень доступа, по умолчанию 'user'
 
-    # Геттеры для получения значений атрибутов
-    def get_user_id(self):
-        return self._user_id
+class Animal:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-    def get_name(self):
-        return self._name
+    def make_sound(self):
+        raise NotImplementedError("Этот метод должен быть переопределен в подклассах")
 
-    def get_access_level(self):
-        return self._access_level
+    def eat(self, food):
+        print(f"{self.name} ест {food}.")
+class Cat(Animal):
+    def make_sound(self):
+        return "Мяу!"
+class Dog(Animal):
+    def make_sound(self):
+        return "Гав!"
 
-        # Сеттеры для изменения значений атрибутов
-    def set_name(self, name):
-        self._name = name
+cat = Cat("Барсик", 3)
+dog = Dog("Шарик", 5)
 
-class Admin(User):
-    def __init__(self, user_id, name):
-        super().__init__(user_id, name)
-        self._access_level = 'admin'  # Уровень доступа администратора
+# Использование методов
+print(f"{cat.name} говорит: {cat.make_sound()}")
+cat.eat("рыбу")
 
-            # Метод для добавления пользователя
-    def add_user(self, user_list, user):
-        user_list.append(user)
-        print(f"Пользователь успешно добавлен.")
-        print(user_list)
-
-            # Метод для удаления пользователя
-    def remove_user(self, user_list, user):
-        user_list.remove(user)
-          # Создаем список для хранения пользователей
-users = []
-admin = Admin(user_id='a1',name='Гоша')
-user1 = User(user_id='U1',name='Спепа')
-print(user1.get_name())
-admin.add_user(users, user1)
-
+print(f"{dog.name} говорит: {dog.make_sound()}")
+dog.eat("мясо")
